de25fc460be04e8d19c6a115d0629c364348888d
Adrien <Adnn@users.noreply.github.com>
2024-11-29 12:26:36 +0100

    Add more buffers to dirty-work-around sound stopping after loads.

--- src/lib/sounds/SoundManager.h
+++ src/lib/sounds/SoundManager.h
@@ -69,7 +69,7 @@ typedef int SoundCategory;
 
 constexpr int MASTER_SOUND_CATEGORY = -1;
 constexpr int HIGHEST_PRIORITY = -1;
-constexpr int BUFFER_PER_CHANNEL = 5;
+constexpr int BUFFER_PER_CHANNEL = 5 * 5;
 constexpr ALsizei MAX_SOURCES = 5;
 const std::size_t MAX_SOURCE_PER_CUE = 3;
 
